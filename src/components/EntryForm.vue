80d72dafdfd88d144e1e6a59ffc78d75946d4e0a
lukefilipos <lfilipos@gmail.com>
2019-04-28 01:21:09 -0500

    second commit

---
+++ src/components/EntryForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Entry Form</h1>
+    <ul>
+      <li v-for="character in characters">{{character.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import CharacterData from '../assets/Characters'
+
+  export default {
+    name: 'EntryForm',
+    data () {
+      return {
+        characters: CharacterData.characters
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Secure.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="secure">
+    <h1>Secure Area</h1>
+    <entry-form></entry-form>
+  </div>
+</template>
+
+<script>
+  import EntryForm from '../components/EntryForm'
+
+  export default {
+    name: 'Secure',
+    components: {
+      EntryForm
+    },
+    data() {
+      return {};
+    }
+  }
+</script>
+
+<style scoped>
+  #secure {
+    background-color: #FFFFFF;
+    border: 1px solid #CCCCCC;
+    padding: 20px;
+    margin-top: 10px;
+  }
+</style>
